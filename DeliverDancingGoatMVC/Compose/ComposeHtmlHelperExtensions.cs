b5b0b0108ebbf977cd6a8ce0d4a78ef5ba155e73
kentico_martinh <martinh@kentico.com>
2016-11-30 17:15:30 +0100

    Better timeout handling

--- DeliverDancingGoatMVC/Compose/ComposeHtmlHelperExtensions.cs
+++ DeliverDancingGoatMVC/Compose/ComposeHtmlHelperExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,20 +13,28 @@ namespace Compose
     {
         private static HttpClient httpClient = new HttpClient();
 
+        private readonly static int TIMEOUT = 30;
         private readonly static string ENDPOINT = ConfigurationManager.AppSettings["ComposeEndpoint"] ?? "http://localhost:59691/";
-
+        
 
         public static async Task<HtmlString> EditableAreaAsync(this HtmlHelper helper, string id)
         {
             var scripts = Scripts.Render(ENDPOINT + "compose.js");
 
             var url = ENDPOINT + "WidgetManager/Zone?design=1&location=data:" + id;
-            var ct = new CancellationTokenSource(1000).Token;
+            var ct = new CancellationTokenSource(TIMEOUT * 1000).Token;
 
-            var content = await httpClient.GetAsync(url, ct).ConfigureAwait(false);
-            var html = await content.Content.ReadAsStringAsync().ConfigureAwait(false);
+            try
+            {
+                var content = await httpClient.GetAsync(url, ct).ConfigureAwait(false);
+                var html = await content.Content.ReadAsStringAsync().ConfigureAwait(false);
 
-            return new HtmlString(scripts + html);
+                return new HtmlString(scripts + html);
+            }
+            catch (Exception ex)
+            {
+                return new HtmlString(ex.Message);
+            }
         }
 
 
