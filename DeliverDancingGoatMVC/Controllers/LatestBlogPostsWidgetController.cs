62e319729ec145d0e4b09517b539093d69e2f3e9
Martin Knapik <knapik.martin92@gmail.com>
2017-02-03 15:21:52 +0100

    CMP-118 - Created Latest blog posts widget from existing Custom Widget so now it gets articles from store and returns them as partial View (similar to Articles page). This is used to populate content for HTTP widget. Also added editable area to bottom of About Us page as per requirements.

---
+++ DeliverDancingGoatMVC/Controllers/LatestBlogPostsWidgetController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Configuration;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using KenticoCloud.Deliver;
+using Newtonsoft.Json.Linq;
+
+namespace DeliverDancingGoatMVC.Controllers
+{
+    public class LatestBlogPostsWidgetController : Controller
+    {
+        private const int DISPLAY_LIMIT = 4;
+        private const string ORDER_ELEMENT = "elements.post_date";
+        private const OrderDirection ORDER_DIRECTION = OrderDirection.Descending;
+
+        private readonly DeliverClient client = new DeliverClient(ConfigurationManager.AppSettings["ProjectId"]);
+
+        [Route("Widgets/LatestBlogPostsWidget")]
+        public async Task<ActionResult> Default(string properties)
+        {
+            var props = JObject.Parse(properties);
+            var filters = new List<IFilter> {
+                new EqualsFilter("system.type", "article"),
+                new Order(ORDER_ELEMENT, ORDER_DIRECTION),
+                new ElementsFilter("teaser_image", "post_date", "summary"),
+                new LimitFilter(DISPLAY_LIMIT)
+            };
+            var articles = await client.GetItemsAsync(filters);
+            
+            return PartialView(articles.Items);
+        }
+    }
+}
