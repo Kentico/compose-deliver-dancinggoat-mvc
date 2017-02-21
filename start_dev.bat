c2e6648bcf30832d6be01b9a783f216f0d636411
kentico_martinh <martinh@kentico.com>
2017-02-21 16:49:33 +0100

    CMP-228 - Move pull to main file, add comment

--- start_dev.bat
+++ start_dev.bat
@@ -1,3 +1,4 @@
-git pull
+:: This batch file serves to quick-start necessary development environment for the current project
+:: %vspath% (Visual Studio path) needs to be provided in environment variables
 
 start "" %vspath% "%cd%\DeliverDancingGoatMVC.sln" /Run
