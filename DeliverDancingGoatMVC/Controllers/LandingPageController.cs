aa6fd7be5ab8ac2252e7331150b07b23b12c3a49
MASTER\MartinM <martinm@kentico.com>
2017-03-02 10:14:35 +0100

    CMP-203 - Landing page controller with views restore

---
+++ DeliverDancingGoatMVC/Controllers/LandingPageController.cs
@@ -0,0 +1,62 @@
+﻿using System.Configuration;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Net;
+using System.Linq;
+
+using KenticoCloud.Deliver;
+
+namespace DeliverDancingGoatMVC.Controllers
+{
+    public class LandingPageController : AsyncController
+    {
+        private readonly DeliverClient client = new DeliverClient(ConfigurationManager.AppSettings["ProjectId"], ConfigurationManager.AppSettings["PreviewToken"]);
+
+
+        public async Task<ActionResult> Empty(string urlSlug)
+        {
+            return await ShowPage(urlSlug, "empty_page", "Empty");
+        }
+
+
+        public async Task<ActionResult> View(string urlSlug)
+        {
+            return await ShowPage(urlSlug, "landing_page");
+        }
+
+
+        private async Task<ActionResult> ShowPage(string urlSlug, string type, string viewName = null)
+        {
+            if (urlSlug == null)
+            {
+                return Redirect("/");
+            }
+
+            try
+            {
+                var response =
+                    await client.GetItemsAsync(new[] {
+                        new EqualsFilter("system.type", type),
+                        new EqualsFilter("elements.url", urlSlug)
+                    });
+
+                var item = response.Items.FirstOrDefault();
+                if (item == null)
+                {
+                    return Redirect("/");
+                }
+
+                return View(viewName, item);
+            }
+            catch (DeliverException ex)
+            {
+                if (ex.StatusCode == HttpStatusCode.NotFound)
+                {
+                    return Redirect("/");
+                }
+
+                throw;
+            }
+        }
+    }
+}
